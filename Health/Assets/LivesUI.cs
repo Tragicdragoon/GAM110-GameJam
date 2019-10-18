53814887323d732a4bbb8e257d04136d9a4bd2ef
Callum <metcalfewood@hotmail.com>
2019-10-18 15:22:55 +0100

    Basic Health System created

---
+++ Health/Assets/LivesUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class LivesUI : MonoBehaviour
+{
+    private float lives = 20;
+
+    public TextMeshProUGUI livesText;
+    void Start()
+    {
+        lives = 20;
+    }
+
+    public void SetLives(int damage)
+    {
+        lives -= damage;
+        livesText.text = lives.ToString() + " Lives";
+    }
+
+}
