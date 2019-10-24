752df4177f9c37d7d42c850982c3bca9054927be
callum <metcalfewood@hotmail.com>
2019-10-24 15:17:11 +0100

    Made the health go down when a zombie reaches the end zone

---
+++ Health/Assets/EndZone.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndZone : MonoBehaviour
+{
+    public LivesUI lUI;
+    public GameObject livesText;
+
+    private void Start()
+    {
+        lUI = livesText.GetComponent<LivesUI>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            
+            Destroy(other.gameObject);
+            lUI.SetLives(1);
+
+        }
+    }
+}
