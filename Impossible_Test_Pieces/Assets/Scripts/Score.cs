033a8e09a37a9431919220851e73fda4ac60e7fa
callum <metcalfewood@hotmail.com>
2019-10-24 17:29:38 +0100

    Added health and score to main game

---
+++ Impossible_Test_Pieces/Assets/Scripts/Score.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    private int score = 0;
+
+    public TextMeshProUGUI scoreText;
+    void Start()
+    {
+        score = 0;
+    }
+
+    public void SmallZombie()
+    {
+        score += 5;
+        scoreText.text = score.ToString();
+    }
+
+    public void MediumZombie()
+    {
+        score += 15;
+        scoreText.text = score.ToString();
+    }
+
+    public void LargeZombie()
+    {
+        score += 50;
+        scoreText.text = score.ToString();
+    }
+}
